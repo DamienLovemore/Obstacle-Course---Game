a60b1f48b1b7d336ec18f57667648a6624abd907
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-10-29 08:15:05 -0300

    Code otimizations for build version

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -14,4 +14,29 @@ public class GameManager : MonoBehaviour
 
         playerCharacter.transform.position = position;
     }
+
+    void Update()
+    {
+        //Toggles screen mode
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            FullScreenMode currentScreenMode = Screen.fullScreenMode;
+
+            //If it is at the fullscreen mode, then it should switch back to windowed mode
+            if ((currentScreenMode != FullScreenMode.Windowed) && (currentScreenMode != FullScreenMode.MaximizedWindow))
+            {
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+            }
+            //If it is at the windowed mode, then it should switch to fullscreen mode
+            else
+            {
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+            }
+        }
+        //Exits the game
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
 }
